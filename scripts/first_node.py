607cf6746010fc80de1cc83262d86320091bd0c7
DivineKenzo <gvozdev2110@bk.ru>
2025-04-01 03:28:06 +0300

    Clock in terminal, ty node in ROS

--- scripts/first_node.py
+++ scripts/first_node.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
-def main():
-    print(f"Hello ROS World!")
-
+def main(): 
+     import datetime
+     import time
+     try:
+          while True:    
+               current_time = datetime.datetime.now()
+               print(current_time.strftime(" %H:%M:%S"), end='\r')
+               time.sleep(5)
+     except KeyboardInterrupt:
+          print(" Действие отменено пользователем")
 
 if __name__ == "__main__":
     main()
